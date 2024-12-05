3053863d083dfbb1e8b2e269724bd6d6f3b11e60
shohagdu <omarshohag93@gmail.com>
2024-12-05 16:55:40 +0600

    update

---
+++ registration.php
@@ -0,0 +1,144 @@
+<!-- Navbar Start -->
+<?php include('menu.php');?>
+<!-- Navbar End -->
+
+<!-- Hero Start -->
+<?php include('banner.php');?>
+<!-- Hero End -->
+
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="row gx-5">
+            <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded" src="img/about.jpg" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="mb-4">
+                    <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">Registration</h5>
+                    <h1 class="display-4">Bangladesh School of Oncoplastic Surgery</h1>
+                </div>
+                <p>Published Very Soon. Thanks</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Topbar End -->
+
+<!---->
+
+<!--<div class="container-fluid sticky-top bg-white shadow-sm mb-5">-->
+<!--    <div class="container">-->
+<!--        <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">-->
+<!--            <a href="index.php" class="navbar-brand">-->
+<!--                <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>Medinova</h1>-->
+<!--            </a>-->
+<!--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">-->
+<!--                <span class="navbar-toggler-icon"></span>-->
+<!--            </button>-->
+<!--            <div class="collapse navbar-collapse" id="navbarCollapse">-->
+<!--                <div class="navbar-nav ms-auto py-0">-->
+<!--                    <a href="index.php" class="nav-item nav-link">Home</a>-->
+<!--                    <a href="about.php" class="nav-item nav-link">About</a>-->
+<!--                    <a href="service.php" class="nav-item nav-link">Service</a>-->
+<!--                    <a href="price.php" class="nav-item nav-link">Pricing</a>-->
+<!--                    <div class="nav-item dropdown">-->
+<!--                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>-->
+<!--                        <div class="dropdown-menu m-0">-->
+<!--                            <a href="blog.php" class="dropdown-item">Blog Grid</a>-->
+<!--                            <a href="detail.php" class="dropdown-item">Blog Detail</a>-->
+<!--                            <a href="team.php" class="dropdown-item">The Team</a>-->
+<!--                            <a href="testimonial.php" class="dropdown-item">Testimonial</a>-->
+<!--                            <a href="appointment.php" class="dropdown-item">Appointment</a>-->
+<!--                            <a href="search.php" class="dropdown-item">Search</a>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <a href="contact.html" class="nav-item nav-link active">Contact</a>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </nav>-->
+<!--    </div>-->
+<!--</div>-->
+
+<!---->
+<!---->
+
+<!--<div class="container-fluid pt-5">-->
+<!--    <div class="container">-->
+<!--        <div class="text-center mx-auto mb-5" style="max-width: 500px;">-->
+<!--            <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">Any Questions?</h5>-->
+<!--            <h1 class="display-4">Please Feel Free To Contact Us</h1>-->
+<!--        </div>-->
+<!--        <div class="row g-5 mb-5">-->
+<!--            <div class="col-lg-4">-->
+<!--                <div class="bg-light rounded d-flex flex-column align-items-center justify-content-center text-center" style="height: 200px;">-->
+<!--                    <div class="d-flex align-items-center justify-content-center bg-primary rounded-circle mb-4" style="width: 100px; height: 70px; transform: rotate(-15deg);">-->
+<!--                        <i class="fa fa-2x fa-location-arrow text-white" style="transform: rotate(15deg);"></i>-->
+<!--                    </div>-->
+<!--                    <h6 class="mb-0">123 Street, New York, USA</h6>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="col-lg-4">-->
+<!--                <div class="bg-light rounded d-flex flex-column align-items-center justify-content-center text-center" style="height: 200px;">-->
+<!--                    <div class="d-flex align-items-center justify-content-center bg-primary rounded-circle mb-4" style="width: 100px; height: 70px; transform: rotate(-15deg);">-->
+<!--                        <i class="fa fa-2x fa-phone text-white" style="transform: rotate(15deg);"></i>-->
+<!--                    </div>-->
+<!--                    <h6 class="mb-0">+012 345 6789</h6>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="col-lg-4">-->
+<!--                <div class="bg-light rounded d-flex flex-column align-items-center justify-content-center text-center" style="height: 200px;">-->
+<!--                    <div class="d-flex align-items-center justify-content-center bg-primary rounded-circle mb-4" style="width: 100px; height: 70px; transform: rotate(-15deg);">-->
+<!--                        <i class="fa fa-2x fa-envelope-open text-white" style="transform: rotate(15deg);"></i>-->
+<!--                    </div>-->
+<!--                    <h6 class="mb-0">info@example.com</h6>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="row">-->
+<!--            <div class="col-12" style="height: 500px;">-->
+<!--                <div class="position-relative h-100">-->
+<!--                    <iframe class="position-relative w-100 h-100"-->
+<!--                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"-->
+<!--                            frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"-->
+<!--                            tabindex="0"></iframe>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="row justify-content-center position-relative" style="margin-top: -200px; z-index: 1;">-->
+<!--            <div class="col-lg-8">-->
+<!--                <div class="bg-white rounded p-5 m-5 mb-0">-->
+<!--                    <form>-->
+<!--                        <div class="row g-3">-->
+<!--                            <div class="col-12 col-sm-6">-->
+<!--                                <input type="text" class="form-control bg-light border-0" placeholder="Your Name" style="height: 55px;">-->
+<!--                            </div>-->
+<!--                            <div class="col-12 col-sm-6">-->
+<!--                                <input type="email" class="form-control bg-light border-0" placeholder="Your Email" style="height: 55px;">-->
+<!--                            </div>-->
+<!--                            <div class="col-12">-->
+<!--                                <input type="text" class="form-control bg-light border-0" placeholder="Subject" style="height: 55px;">-->
+<!--                            </div>-->
+<!--                            <div class="col-12">-->
+<!--                                <textarea class="form-control bg-light border-0" rows="5" placeholder="Message"></textarea>-->
+<!--                            </div>-->
+<!--                            <div class="col-12">-->
+<!--                                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </form>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+
+
+
+
+
+<!-- Footer Start -->
+<?php include('footer.php');?>
+<!-- Footer End -->
