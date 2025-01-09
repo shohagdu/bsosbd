51f3c42ffff67b1f1f0d817b2ee06ea2175b8a05
shohagdu <omarshohag93@gmail.com>
2025-01-09 11:54:15 +0600

    update mobile number

--- resources/views/frontDirectory/layouts/master.blade.php
+++ resources/views/frontDirectory/layouts/master.blade.php
@@ -37,8 +37,8 @@
         <div class="row">
             <div class="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
                 <div class="d-inline-flex align-items-center">
-                    <a class="text-decoration-none text-body pe-3" href=""><i class="bi bi-telephone me-2"></i>+880 1711 161199</a>
-                    <span class="text-body">|</span>
+{{--                    <a class="text-decoration-none text-body pe-3" href=""><i class="bi bi-telephone me-2"></i>+880 1711 161199</a>--}}
+{{--                    <span class="text-body">|</span>--}}
                     <a class="text-decoration-none text-body px-3" href=""><i class="bi bi-envelope me-2"></i>info@bsosbd.com</a>
                 </div>
             </div>
@@ -65,13 +65,13 @@
 <div class="container-fluid bg-dark text-light mt-5 py-5">
     <div class="container py-5">
         <div class="row g-5">
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-4 col-md-6">
                 <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Get In Touch</h4>
                 <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Bangladesh Cancer Society, 120/3 Darus Salam, Mirpur, Bangladesh</p>
                 <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@bsosbd.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+880 1711 161199</p>
+{{--                <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+880 1711 161199</p>--}}
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-4 col-md-6">
                 <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Quick Links</h4>
                 <div class="d-flex flex-column justify-content-start">
                     <a class="text-light mb-2" href="{{ url('/') }}"><i class="fa fa-angle-right me-2"></i>Home</a>
@@ -82,26 +82,26 @@
                     <a class="text-light" href="{{ url('/contact') }}"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Popular Links</h4>
-                <div class="d-flex flex-column justify-content-start">
-                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>-->
-                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>-->
-                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>-->
-                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>-->
-                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>-->
-                    <!--                    <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>-->
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Newsletter</h4>
-                <form action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control p-3 border-0" placeholder="Your Email Address">
-                        <button class="btn btn-primary">Sign Up</button>
-                    </div>
-                </form>
-                <h6 class="text-primary text-uppercase mt-4 mb-3">Follow Us</h6>
+{{--            <div class="col-lg-3 col-md-6">--}}
+{{--                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Popular Links</h4>--}}
+{{--                <div class="d-flex flex-column justify-content-start">--}}
+{{--                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>-->--}}
+{{--                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>-->--}}
+{{--                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>-->--}}
+{{--                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>-->--}}
+{{--                    <!--                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>-->--}}
+{{--                    <!--                    <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>-->--}}
+{{--                </div>--}}
+{{--            </div>--}}
+            <div class="col-lg-4 col-md-6">
+                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Follow Us</h4>
+{{--                <form action="">--}}
+{{--                    <div class="input-group">--}}
+{{--                        <input type="text" class="form-control p-3 border-0" placeholder="Your Email Address">--}}
+{{--                        <button class="btn btn-primary">Sign Up</button>--}}
+{{--                    </div>--}}
+{{--                </form>--}}
+{{--                <h6 class="text-primary text-uppercase mt-4 mb-3">Follow Us</h6>--}}
                 <div class="d-flex">
                     <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" target="_blank" href="https://www.facebook.com/profile.php?id=100063763461416"><i class="fab fa-facebook-f"></i></a>
                 </div>
