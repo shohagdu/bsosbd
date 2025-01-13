2d0ce8ca8b62fac0d2e98940895467d22391405e
shohagdu <omarshohag93@gmail.com>
2025-01-13 16:09:51 +0600

    update some issue abdullah

--- resources/views/frontDirectory/viewFacultyDetails.blade.php
+++ resources/views/frontDirectory/viewFacultyDetails.blade.php
@@ -30,74 +30,12 @@
                     <p class="m-0" style="color: #333;">
 {{--                        {{ $facultyMember->institute??NULL }}--}}
                         {!! $facultyMember->biography??NULL  !!}
-                        adfas
                     </p>
                 </div>
             </div>
         </div>
     </div>
     <!-- Blog End -->
-
-
-
-    <div class="container-fluid py-5">
-        <div class="container" style="color: #333;">
-            <div class="row gx-5">
-                <div class="col-lg-12">
-                    <div class="row justify-content-center position-relative" >
-                        <div class="col-lg-12">
-                            <div class="">
-                                <table class="table table-bordered" style="width: 100%;color:#333;">
-                                    <tr>
-                                        <td colspan="2" style="font-weight: bold;font-size:20px;">Please Check Before Payment</td>
-                                    </tr>
-                                    <tr>
-                                        <th style="width: 30%;">Name</th>
-                                        <td>     {{$facultyMember->name??NULL}}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Institute</th>
-                                        <td>{{$facultyMember->institute??NULL}}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Degree</th>
-                                        <td>{{$facultyMember->degree??NULL}}</td>
-                                    </tr>
-
-                                    <tr>
-                                        <th>Mobile</th>
-                                        <td>{{$facultyMember->mobile??NULL}}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Email</th>
-                                        <td>{{$facultyMember->email??NULL}}</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" style="text-align: center">
-                                            <a href="{{ url('/') }}" class="btn btn-danger">Back</a>
-                                        </td>
-                                    </tr>
-
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <style>
-        #levelFontSize{
-            font-size: 20px;
-            font-weight: bold;
-        }
-        #showAmount{
-            font-weight: bold;
-            font-size: 22px;
-            color:red;
-        }
-
-    </style>
 @endsection
 
 
